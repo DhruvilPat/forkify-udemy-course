e563d10d3682d27aa8f26534a0fb8a35a2bed56e
captian-india <dhruvil.gloryautotech@gmail.com>
2022-08-08 18:16:33 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c2ce7394-96d0-4cf1-9a0b-7858184beef2';
+export const MODAL_CLOSE_SEC = 2.5;
